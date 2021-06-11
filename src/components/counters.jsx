1a5a520a08660135015686d2d031268977332e4b
Kamruzzaman Emon <57123418+kemonsense@users.noreply.github.com>
2021-06-11 12:33:39 +0600

    stateless function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,27 +2,18 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 6 },
-      { id: 2, value: 0 },
-      { id: 3, value: 5 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterID) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterID);
-    this.setState({ counters });
-  };
-
   render() {
+    const { counters, onReset, onDelete, onIncrement } = this.props;
     return (
       <div>
-        {this.state.counters.map((counter) => (
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
